fb665d1b749a20431013bcfb9842a7ff8069a8ed
Julia Marcinkowska <jmarcinkowska@gmail.com>
2021-03-13 15:04:56 +0100

    add prints to be more user friendly

--- favgen.py
+++ favgen.py
@@ -1,6 +1,6 @@
-# list of tier files
+from urllib.parse import unquote
+
 # this list determines the order in which the tiers will be displayed
-# you can remove or add files if you want
 tier_files = [
     "S_tier.txt",
     "A_tier.txt",
@@ -12,17 +12,36 @@ tier_files = [
 ]
 output_file = "favbbcode.txt"
 
+title = "MAL Favorites BBCode Generator"
+print(f"\t|{len(title)*'-'}|\n\t|{title}|\n\t|{len(title)*'-'}|")
+
 with open(output_file, "w") as output:
-    for tier_file in tier_files:
-        with open(tier_file, "r") as f:
-            lines = f.read().splitlines()
-            tier_banner = lines[0]
-            tier_characters = lines[1:]
+    for i, tier_file in enumerate(tier_files, start=1):
+        try:
+            with open(tier_file, "r") as f:
+                lines = f.read().splitlines()
+                tier_banner = lines[0].strip()
+                tier_characters_lines = lines[1:]
 
-            output.write(f"[img]{tier_banner}[/img]\n")
+                output.write(f"[img]{tier_banner}[/img]\n")
 
-            for character in tier_characters:
-                img, mal = character.split(",")
-                output.write(f"[url={mal}][img]{img}[/img][/url]")
+                tier_characters = []
+                for character in tier_characters_lines:
+                    img, mal = map(lambda x: x.strip(), character.split(","))
+                    tier_characters.append(
+                        "\t" + unquote(mal.split("/")[-1]).replace("_", " "))
+                    output.write(f"[url={mal}][img]{img}[/img][/url]")
 
-            output.write("\n")
+                output.write("\n")
+
+            print(f"[{i}/{len(tier_files)}] BBCode for file {tier_file} "
+                  f"generated successfuly.")
+
+            if tier_characters:
+                print("\n".join(tier_characters))
+
+        except FileNotFoundError:
+            print(f"[!][{i}/{len(tier_files)}] File {tier_file} not found! "
+                  "No BBCode was generated.")
+
+print(f"Generated BBCode is in file {output_file}")
