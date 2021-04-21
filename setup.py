17440f3e7a8148e643851512914c2c5e6bcca9fe
Julia Marcinkowska <jmarcinkowska@gmail.com>
2021-04-21 16:05:06 +0200

    add setup.py and tests for Image

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+
+entry_points = {
+    'console_scripts': [
+        'mal_fav_bbcode_gen = mal_fav_bbcode_gen.cli:main',
+    ]
+}
+
+setuptools.setup(
+    name='mal-fav-bbcode-gen',
+    version='0.1',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    description='BBCode generator for MyAnimeList blog post',
+    url='https://github.com/juliamarc/mal-fav-bbcode-gen',
+    # TODO look up in documentation
+    install_requires=['click', 'bbcode', 'ezodf', 'lxml'],
+    entry_points=entry_points,
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ]
+)
---
+++ test/test_image.py
@@ -0,0 +1,42 @@
+import pytest
+
+from mal_fav_bbcode_gen.image import Image
+
+
+def test_source_direct_url():
+    image_url = 'example.com/test.png'
+    image = Image('direct URL', image_url)
+
+    assert image.image_url == image_url
+
+
+def test_source_google_drive_file_id():
+    expected_url = ('https://drive.google.com/uc'
+                    '?id=1olKc6TUJ1kPJa7cKWVp7dNZFwHb_0k8Z')
+    image_url = '1olKc6TUJ1kPJa7cKWVp7dNZFwHb_0k8Z'
+    image = Image('Google Drive', image_url)
+
+    assert image.image_url == expected_url
+
+
+def test_source_google_drive_share_link():
+    expected_url = ('https://drive.google.com/uc'
+                    '?id=1olKc6TUJ1kPJa7cKWVp7dNZFwHb_0k8Z')
+    image_url = ('https://drive.google.com/file/d/'
+                 '1olKc6TUJ1kPJa7cKWVp7dNZFwHb_0k8Z/view?usp=sharing')
+    image = Image('Google Drive', image_url)
+
+    assert image.image_url == expected_url
+
+
+def test_source_not_valid():
+    with pytest.raises(KeyError, match=r".*is not a valid image source.*"):
+        Image('not valid', 'example.com/test.png')
+
+
+def test_get_bbcode():
+    image_url = 'example.com/test.png'
+    expected_bbcode = f'[img]{image_url}[/img]'
+    image = Image('direct URL', image_url)
+
+    assert image.get_bbcode() == expected_bbcode
