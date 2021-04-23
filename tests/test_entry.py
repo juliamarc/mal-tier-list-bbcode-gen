18d6d5c2c95bc2303f9898ddeb8ca9a0c980b141
Julia Marcinkowska <jmarcinkowska@gmail.com>
2021-04-23 22:05:44 +0200

    add fixtures and parametrized test to entry tests

--- tests/test_entry.py
+++ tests/test_entry.py
@@ -1,49 +1,47 @@
+import pytest
+
 from mal_tier_list_bbcode_gen.entry import Entry
 
 
-def test_name_one_word():
-    expected_name = 'Zeke'
-    mal_url = 'https://myanimelist.net/character/142314/Zeke'
-    image_url = 'example.com/test.png'
-    entry = Entry(mal_url, 'direct URL', image_url)
+@pytest.fixture(autouse=True)
+def direct_image_url():
+    return 'example.com/test.png'
+
+
+@pytest.mark.parametrize(
+    "mal_url,expected_name",
+    [
+        pytest.param(
+            'https://myanimelist.net/character/142314/Zeke',
+            'Zeke',
+            id="one_word"
+        ),
+        pytest.param(
+            'https://myanimelist.net/character/164471/Satoru_Gojou',
+            'Satoru Gojou',
+            id="two_words"
+        ),
+        pytest.param(
+            'https://myanimelist.net/character/3934/Olivier_Mira_Armstrong',
+            'Olivier Mira Armstrong',
+            id="three_words"
+        ),
+        pytest.param(
+            'https://myanimelist.net/character/71121/Hange_Zo%C3%AB',
+            'Hange Zoë',
+            id="unquote"
+        ),
+    ],
+)
+def test_name(mal_url, expected_name):
+    entry = Entry(mal_url, 'direct URL', direct_image_url)
 
     assert entry.name == expected_name
-    assert str(entry) == expected_name
-
-
-def test_name_two_words():
-    expected_name = 'Satoru Gojou'
-    mal_url = 'https://myanimelist.net/character/164471/Satoru_Gojou'
-    image_url = 'example.com/test.png'
-    entry = Entry(mal_url, 'direct URL', image_url)
-
-    assert entry.name == expected_name
-    assert str(entry) == expected_name
-
-
-def test_name_three_words():
-    expected_name = 'Olivier Mira Armstrong'
-    mal_url = 'https://myanimelist.net/character/3934/Olivier_Mira_Armstrong'
-    image_url = 'example.com/test.png'
-    entry = Entry(mal_url, 'direct URL', image_url)
-
-    assert entry.name == expected_name
-    assert str(entry) == expected_name
-
-
-def test_name_unquote():
-    expected_name = 'Hange Zoë'
-    mal_url = 'https://myanimelist.net/character/71121/Hange_Zo%C3%AB'
-    image_url = 'example.com/test.png'
-    entry = Entry(mal_url, 'direct URL', image_url)
-
-    assert entry.name == expected_name
-    assert str(entry) == expected_name
 
 
 def test_get_bbcode():
     mal_url = 'https://myanimelist.net/character/142314/Zeke'
-    entry = Entry(mal_url, 'direct URL', 'example.com/test.png')
+    entry = Entry(mal_url, 'direct URL', direct_image_url)
     expected_bbcode = f'[url={mal_url}][img]{entry.image_url}[/img][/url]'
 
     assert entry.get_bbcode() == expected_bbcode
