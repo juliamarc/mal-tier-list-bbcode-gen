2f2c818626f0b71b89020d330b56391f8c8b79b4
Julia Marcinkowska <jmarcinkowska@gmail.com>
2021-04-24 16:09:36 +0200

    add tests for new utils function

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -28,5 +28,40 @@ from mal_tier_list_bbcode_gen import utils
         ),
     ],
 )
-def test_dedup_list(input_list, output_list):
+def test_deduplicate_list(input_list, output_list):
     assert utils.deduplicate_list(input_list) == output_list
+
+
+@pytest.mark.parametrize(
+    "input_list,filter_set,expected_list",
+    [
+        pytest.param(
+            [1, 2, 3, 4, 5],
+            set(),
+            [1, 2, 3, 4, 5],
+            id="empty_set"
+        ),
+        pytest.param(
+            [1, 2, 3, 4, 5],
+            set([2, 4]),
+            [1, 3, 5],
+            id="two_elem_set"
+        ),
+        pytest.param(
+            [],
+            set(),
+            [],
+            id="all_empty"
+        ),
+        pytest.param(
+            [2, 1, 3, 7],
+            set([2, 1, 3, 7]),
+            [],
+            id="all_filtered"
+        ),
+    ],
+)
+def test_filter_list_by_set(input_list, filter_set, expected_list):
+    filtered_list = utils.filter_list_by_set(input_list, filter_set)
+
+    assert filtered_list == expected_list
