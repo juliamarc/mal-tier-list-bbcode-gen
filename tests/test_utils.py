03ffb602b8946ae43ac63bd9b5339c32031af02e
Julia Marcinkowska <jmarcinkowska@gmail.com>
2021-04-24 12:41:36 +0200

    parametrize test for dedup list

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,9 +1,32 @@
+import pytest
+
 from mal_tier_list_bbcode_gen import utils
 
 
-def test_dedup_list_with_dups():
-    assert utils.deduplicate_list([7, 6, 2, 2, 3, 6, 4, 6]) == [7, 6, 2, 3, 4]
-
-
-def test_dedup_list_with_no_dups():
-    assert utils.deduplicate_list([2, 1, 3, 7]) == [2, 1, 3, 7]
+@pytest.mark.parametrize(
+    "input_list,output_list",
+    [
+        pytest.param(
+            [1, 2, 3, 4, 5],
+            [1, 2, 3, 4, 5],
+            id="ordered_no_dups"
+        ),
+        pytest.param(
+            [1, 1, 2, 2, 3, 3, 4, 4, 5, 5],
+            [1, 2, 3, 4, 5],
+            id="ordered_dups"
+        ),
+        pytest.param(
+            [2, 1, 3, 7],
+            [2, 1, 3, 7],
+            id="maintain_order_no_dups"
+        ),
+        pytest.param(
+            [2, 1, 3, 7, 7, 3, 1, 2, 8],
+            [2, 1, 3, 7, 8],
+            id="maintain_order_dups"
+        ),
+    ],
+)
+def test_dedup_list(input_list, output_list):
+    assert utils.deduplicate_list(input_list) == output_list
