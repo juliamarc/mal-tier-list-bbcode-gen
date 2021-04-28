6e6defb6e82b41059d028eef22e498718651265a
Julia Marcinkowska <jmarcinkowska@gmail.com>
2021-04-28 17:19:59 +0200

    add MAL URL validation to Entry

--- tests/test_entry.py
+++ tests/test_entry.py
@@ -1,5 +1,7 @@
 import pytest
 
+import mal_tier_list_bbcode_gen.exceptions as exceptions
+
 from mal_tier_list_bbcode_gen.entry import Entry
 
 
@@ -37,6 +39,39 @@ def test_name(mal_url, expected_name):
     entry = Entry(mal_url, 'direct URL', direct_image_url)
 
     assert entry.name == expected_name
+
+
+@pytest.mark.parametrize(
+    "mal_url",
+    [
+        pytest.param(
+            'https://myanimelist.net/character/142314/Zeke',
+            id="character"
+        ),
+        pytest.param(
+            'https://myanimelist.net/anime/39026/Dumbbell_Nan_Kilo_Moteru',
+            id="anime"
+        ),
+        pytest.param(
+            'https://myanimelist.net/manga/113138/Jujutsu_Kaisen',
+            id="manga"
+        ),
+        pytest.param(
+            'https://myanimelist.net/people/212/Daisuke_Ono',
+            id="people"
+        ),
+    ],
+)
+def test_validate_mal_url_valid_url(mal_url):
+    entry = Entry(mal_url, 'direct URL', direct_image_url)
+
+    assert entry.mal_url == mal_url
+
+
+def test_validate_mal_url_invalid_url():
+    invalid_mal_url = 'https://www.google.com/'
+    with pytest.raises(exceptions.InvalidMALURL):
+        Entry(invalid_mal_url, 'direct URL', direct_image_url)
 
 
 def test_get_bbcode():
